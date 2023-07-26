86f1266fa7abb0de2cc6a106c78674038c84d935
Taiye Ogunlade <taiyeo@banquemisr.com>
2023-07-26 12:49:13 +0100

    #Find in Matrix

---
+++ src/com/taiye/ogunlade/java/kunal/binarysearch2D/RowColMatrix.java
@@ -0,0 +1,34 @@
+package com.taiye.ogunlade.java.kunal.binarysearch2D;
+
+import java.util.Arrays;
+
+public class RowColMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 2, 3, 4},
+                {5, 6, 8, 5},
+                {7, 10, 9, 3}
+        };
+
+        int[] result = searchIn2DArray(matrix, 8);
+        System.out.println("Result " + Arrays.toString(result));
+    }
+
+    static int[] searchIn2DArray(int[][] matrix, int target) {
+
+        int r = 0;
+        int c = matrix.length - 1;
+
+        while (r < matrix.length && c >= 0) {
+            if (matrix[r][c] == target) {
+                return new int[]{r, c};
+            }
+            if (matrix[r][c] < target) {
+                r++;
+            } else {
+                c--;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
